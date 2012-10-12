067683d3ba17e7f7097ee3626f3b5145e28f059b
tatsiana salikayeva <rijik1@list.ru>
2012-10-12 17:35:45 +0300

    add new html structure

---
+++ js/profile_info.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $(function(){
+  	$("#sharmila_s").LePopup({
+  		skin : "big-shadow"
+  	});
+  });
+  $(function(){
+  	$("#john_c").LePopup({
+  		skin : "big-shadow"
+  	});
+  });
+  $(function(){
+  	$("#vaibhav_n").LePopup({
+  		skin : "big-shadow"
+  	});
+  });
+  $(function(){
+  	$("#douglas_vm").LePopup({
+  		skin : "big-shadow"
+  	});
+  });
+});
